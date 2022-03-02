9bf4d8d3ad29f1156ec50c809b4457c94b6daebe
gustlf063@gamil.com <gustlf063@gmail.com>
2022-03-02 12:46:35 +0900

    'add'

--- board/forms.py
+++ board/forms.py
@@ -6,4 +6,8 @@ class QuestionForm(forms.ModelForm):
     class Meta:  # 내부 클래스
         model = Question
         fields = ['subject', 'content']
+        labels = {
+            'subject':'제목',
+            'content':'내용',
+        }
 
