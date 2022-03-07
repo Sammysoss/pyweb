2bb6e09b627129df6ba768315000d932850b4b3e
gustlf063@gamil.com <gustlf063@gmail.com>
2022-03-07 12:46:19 +0900

    'add'

--- board/models.py
+++ board/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
-    create_date = models.DateTimeField()
+    create_date = models.DateTimeField()  # not null
+    modify_date = models.DateTimeField(null=True, blank=True) # null 값 허용/ blank- 폼 데이터null
 
     # user의 username과 외래키 설정
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,6 +16,7 @@ class Question(models.Model):
 class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
